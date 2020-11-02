99917471d07bb2ec487660b0d5c19813e074191a
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-11-02 10:38:48 +0100

    Fix linter errors and add some debug log

--- reviewer/run.sh
+++ reviewer/run.sh
@@ -4,4 +4,7 @@ echo "Starting review ..."
 echo "Workspace:" ${WORKSPACE}
 
 node node_modules/editorconfig-checker/dist/index.js -config ./.ecrc ${WORKSPACE}
-node node_modules/eslint/bin/eslint.js --ext .ts,.vue --no-eslintrc -c ./.eslintrc.json ${WORKSPACE}
+echo "Exit: " $?
+
+node node_modules/eslint/bin/eslint.js --ext .ts, .vue --no-eslintrc --no-error-on-unmatched-pattern -c ./.eslintrc.json ${WORKSPACE}
+echo "Exit: " $?
