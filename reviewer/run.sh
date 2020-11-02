afe7aaaf7388eff8f3e55d5d38bea2262f172045
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-11-02 11:17:59 +0100

    Return exit code

--- reviewer/run.sh
+++ reviewer/run.sh
@@ -4,7 +4,15 @@ echo "Starting review ..."
 echo "Workspace:" ${WORKSPACE}
 
 node node_modules/editorconfig-checker/dist/index.js -config ./.ecrc ${WORKSPACE}
-echo "Exit: " $?
+editorconfig_status=$?
+echo "Exit:" $editorconfig_status
 
 node node_modules/eslint/bin/eslint.js --ext .ts, .vue --no-eslintrc --no-error-on-unmatched-pattern -c ./.eslintrc.json ${WORKSPACE}
-echo "Exit: " $?
+eslint_status=$?
+echo "Exit:" $eslint_status
+
+if [ $editorconfig_status != 0 ] || [ $eslint_status != 0 ]
+then
+    echo "Violations found! Exiting ..."
+    exit 1
+fi
